3690e4cb3e9c35bab0f7e176daae7e5938c6fe36
inceisciberkay <inceisciberkay@gmail.com>
2024-04-21 11:52:33 +0300

    initialize server && implement health check endpoint

--- visit-analytics/src/main.rs
+++ visit-analytics/src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use lokate_visit_analytics::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:4444").expect("Failed to bind");
+    run(listener)?.await
 }
