691d907db702da759f62d2f5db2270382903b29b
inceisciberkay <inceisciberkay@gmail.com>
2024-05-09 22:42:24 +0300

    format

--- src/routes/generate_purchase_analytics.rs
+++ src/routes/generate_purchase_analytics.rs
@@ -16,13 +16,18 @@ pub async fn generate_purchase_analytics(path: web::Path<String>) -> Result<impl
 
         categories_to_probabilities
             .sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap_or(std::cmp::Ordering::Equal));
-        let top_categories: Vec<String> = categories_to_probabilities
-            .into_iter()
-            .take(4)
-            .map(|(category, _)| category)
-            .collect();
+        // let top_categories: Vec<String> = categories_to_probabilities
+        //     .into_iter()
+        //     .take(4)
+        //     .map(|(category, _)| category)
+        //     .collect();
 
-        let top_categories = vec![String::from("bebek"), String::from("kuruyemis"), String::from("icecek"), String::from("kahve")];
+        let top_categories = vec![
+            String::from("bebek"),
+            String::from("kuruyemis"),
+            String::from("icecek"),
+            String::from("kahve"),
+        ];
         log::info!("{top_categories:?}");
         Ok(top_categories)
     });
